3505c7a327fcc8a88da101a7176d8c2eae2d2e70
Tymofii Antoniuk <111820939+timaantonuk@users.noreply.github.com>
2023-03-20 22:11:05 -0500

    cosmetic header changes

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import AboutUs from "../components/AboutUs";
-import Header from "../components/Header";
-import Services from "../components/Services";
-import Reviews from "../components/Reviews";
-import GalleryComponent from "../components/GalleryComponent";
-import ApplyForm from "./ApplyForm";
-import ContactUs from "../components/ContactUs";
+import React from 'react';
+import AboutUs from '../components/AboutUs';
+import Header from '../components/Header';
+import Services from '../components/Services';
+import Reviews from '../components/Reviews';
+import GalleryComponent from '../components/GalleryComponent';
+import ApplyForm from './ApplyForm';
+import ContactUs from '../components/ContactUs';
+import Footer from '../components/Footer';
 
 const MainPage = () => {
   return (
@@ -17,6 +18,7 @@ const MainPage = () => {
       <GalleryComponent />
       <ApplyForm />
       <ContactUs />
+      <Footer />
     </>
   );
 };
