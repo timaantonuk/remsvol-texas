da80a916240081c2a1c691bbb4eed735c7b0fc6d
Tymofii Antoniuk <111820939+timaantonuk@users.noreply.github.com>
2023-03-25 04:06:50 -0500

    fixed some bugs

--- src/components/GalleryComponent.jsx
+++ src/components/GalleryComponent.jsx
@@ -1,12 +1,11 @@
-import React, { useState, useCallback } from "react";
-import Gallery from "react-photo-gallery";
-import Carousel, { Modal, ModalGateway } from "react-images";
-import { BeatLoader } from "react-spinners";
+import React, { useState, useCallback } from 'react';
+import Gallery from 'react-photo-gallery';
+import Carousel, { Modal, ModalGateway } from 'react-images';
+import { BeatLoader } from 'react-spinners';
 
 function GalleryComponent() {
   const [currentImage, setCurrentImage] = useState(0);
   const [viewerIsOpen, setViewerIsOpen] = useState(false);
-  const [loading, setLoading] = useState(true);
 
   const openLightbox = useCallback((event, { photo, index }) => {
     setCurrentImage(index);
@@ -18,142 +17,128 @@ function GalleryComponent() {
     setViewerIsOpen(false);
   };
 
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768); // assuming mobile breakpoint is 768px
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
   const photos = [
     {
-      smallSrc: "/img/gallery-min.jpg",
+      smallSrc: '/img/gallery-min.jpg',
 
       width: 1,
       height: 1,
       modal: {
-        src: isMobile ? '/img/gallery-min.jpg' : '/img/gallery1.jpeg',
+        src: '/img/gallery-min.jpg',
       },
     },
     {
-      smallSrc: "/img/gallery-image-1-min.jpg",
+      smallSrc: '/img/gallery-image-1-min.jpg',
       width: 1,
       height: 1,
       modal: {
-        src: "/img/gallery-image-1.jpg",
+        src: '/img/gallery-image-1.jpg',
       },
     },
     {
-      smallSrc: "/img/gallery-image-2-min.jpg",
+      smallSrc: '/img/gallery-image-2-min.jpg',
       width: 1,
       height: 1,
       modal: {
-        src: "/img/gallery-image-2.jpg",
+        src: '/img/gallery-image-2.jpg',
       },
     },
     {
-      smallSrc: "/img/gallery-image-3-min.jpg",
+      smallSrc: '/img/gallery-image-3-min.jpg',
       width: 1,
       height: 1,
       modal: {
-        src: "/img/gallery-image-3.jpg",
+        src: '/img/gallery-image-3.jpg',
       },
     },
     {
-      smallSrc: "/img/gallery-image-4-min.jpg",
+      smallSrc: '/img/gallery-image-4-min.jpg',
       width: 1,
       height: 1,
       modal: {
-        src: "/img/gallery-image-4.jpg",
+        src: '/img/gallery-image-4.jpg',
       },
     },
     {
-      smallSrc: "/img/gallery-image-5-min.jpg",
+      smallSrc: '/img/gallery-image-5-min.jpg',
       width: 1,
       height: 1,
       modal: {
-        src: "/img/gallery-image-5.jpg",
+        src: '/img/gallery-image-5.jpg',
       },
     },
     {
-      smallSrc: "/img/gallery-image-6-min.jpg",
+      smallSrc: '/img/gallery-image-6-min.jpg',
       width: 1,
       height: 1,
       modal: {
-        src: "/img/gallery-image-6.jpg",
+        src: '/img/gallery-image-6.jpg',
       },
     },
     {
-      smallSrc: "/img/gallery-image-7-min.jpg",
+      smallSrc: '/img/gallery-image-7-min.jpg',
       width: 1,
       height: 1,
       modal: {
-        src: "/img/gallery-image-7.jpg",
+        src: '/img/gallery-image-7.jpg',
       },
     },
     {
-      smallSrc: "/img/gallery-image-8-min.jpg",
+      smallSrc: '/img/gallery-image-8-min.jpg',
       width: 1,
       height: 1,
       modal: {
-        src: "/img/gallery-image-8.jpg",
+        src: '/img/gallery-image-8.jpg',
       },
     },
     {
-      smallSrc: "/img/gallery-image-9.jpeg",
+      smallSrc: '/img/gallery-image-9.jpeg',
       width: 1,
       height: 1,
       modal: {
-        src: "/img/gallery-image-9.jpeg",
+        src: '/img/gallery-image-9.jpeg',
       },
     },
     {
-      smallSrc: "/img/gallery-image-10-min.jpg",
+      smallSrc: '/img/gallery-image-10-min.jpg',
       width: 1,
       height: 1,
       modal: {
-        src: "/img/gallery-image-10.jpg",
+        src: '/img/gallery-image-10.jpg',
       },
     },
     {
-      smallSrc: "/img/gallery-image-11-min.jpg",
+      smallSrc: '/img/gallery-image-11-min.jpg',
       width: 1,
       height: 1,
       modal: {
-        src: "/img/gallery-image-11.jpg",
+        src: '/img/gallery-image-11.jpg',
       },
     },
     {
-      smallSrc: "/img/gallery-image-12-min.jpg",
+      smallSrc: '/img/gallery-image-12-min.jpg',
       width: 1,
       height: 1,
       modal: {
-        src: "/img/gallery-image-12.jpg",
+        src: '/img/gallery-image-12.jpg',
       },
     },
   ];
 
   return (
-    <div className="w-full bg-[#ebe0bf] px-0 lg:p-0 flex items-center justify-center pt-0 lg:pt-0">
-      <div className="gallery-section w-full lg:w-full bg-opacity-20 rounded-none lg:rounded-3xl p-4 ">
-        <h1 className="lg:hidden text-3xl flex justify-center pb-4 pt-8 font-bold text-[#392820]">
+    <div className='w-full bg-[#ebe0bf] px-0 lg:p-0 flex items-center justify-center pt-0 lg:pt-0'>
+      <div className='gallery-section w-full lg:w-full bg-opacity-20 rounded-none lg:rounded-3xl p-4 '>
+        <h1 className='lg:hidden text-3xl flex justify-center pb-4 pt-8 font-bold text-[#392820]'>
           Our gallery
         </h1>
-        <p className="lg:hidden flex justify-center mb-4">Click to zoom 🔍︎</p>
-        <div className="gallery-images">
+        <p className='lg:hidden flex justify-center mb-4'>Click to zoom 🔍︎</p>
+        <div className='gallery-images'>
           <Gallery
             photos={photos.map((el) => ({ src: el.smallSrc, ...el }))}
             onClick={openLightbox}
           />
         </div>
-        <div className="flex justify-center" id="brokephotos">
+        <div className='flex justify-center' id='brokephotos'>
           <ModalGateway>
             {viewerIsOpen ? (
               <Modal onClose={closeLightbox}>
